a53b712c69314696add4bed373851145dc6914dd
Samantha <gsamguerrero@outlook.es>
2020-04-22 17:55:50 +0200

    Nuevo proyecto en JavaFX

--- JavaFX Prueba/src/application/Main.java
+++ JavaFX Prueba/src/application/Main.java
@@ -1,7 +1,37 @@
-import java.sql.*;
+package application;
+	
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public static void conectarBDD() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 
@@ -30,6 +60,5 @@ public class Main {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
 	}
 }
