f5d707b1be34f5f18969d3bd2908b762c423f9c4
Samantha <gsamguerrero@outlook.es>
2020-05-07 18:58:09 +0200

    Exportacion e Importacion igualadas | Ficheros de Pruebas

--- JavaFX Prueba/src/application/Convertidor.java
+++ JavaFX Prueba/src/application/Convertidor.java
@@ -6,26 +6,17 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 import javax.swing.JOptionPane;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.json.JsonHierarchicalStreamDriver;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
 
@@ -112,54 +103,28 @@ public class Convertidor {
 	}
 	
 	public ArrayList<Proveedor> leerProveedoresXML(String ruta) {
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		ArrayList<Proveedor> proveedores = new ArrayList<>();
-		
-		 try {	 
-		    DocumentBuilder builder = factory.newDocumentBuilder();
-		    Document document = (Document) builder.parse(new File(ruta));
-		    document.getDocumentElement().normalize();
 
-		    //crea una lista con todos los nodos empleado  
-		    NodeList empleados = ((org.w3c.dom.Document) document).getElementsByTagName("proveedor");
-		    Proveedor prov;
-		    
-		    //recorrer la lista  
-		    for (int i = 0; i < empleados.getLength(); i ++)
-		    {
-		    	Node emple = empleados.item(i); //obtener un nodo empleado
-		    	if (emple.getNodeType() == Node.ELEMENT_NODE)//tipo de nodo
-		    	{
-		    		//obtener los elementos del nodo           
-		    		Element elemento = (Element) emple;	
-		    		prov = new Proveedor();
-		    		prov.setCif_proveedor(elemento.getElementsByTagName("cif_proveedor").item(0).getTextContent());
-		    		prov.setFec_homologacion(StringToDate(elemento.getElementsByTagName("fec_homologacion").item(0).getTextContent()));
-		    		prov.setRaz_proveedor(elemento.getElementsByTagName("raz_proveedor").item(0).getTextContent());
-		    		prov.setReg_notarial(Integer.parseInt(elemento.getElementsByTagName("reg_notarial").item(0).getTextContent()));
-		    		prov.setSeg_importe(Float.parseFloat(elemento.getElementsByTagName("seg_importe").item(0).getTextContent()));
-		    		prov.setSeg_responsabilidad(Integer.parseInt(elemento.getElementsByTagName("seg_responsabilidad").item(0).getTextContent()));
-		    		
-		    		proveedores.add(prov);
-		    	}
-		    }
-		    
-		  } catch (Exception e) {
-			  e.printStackTrace();
-		  }
+		//Inicializo XStream
+		XStream xstream = new XStream(new DomDriver());
+		xstream.alias("proveedores", ListaProveedores.class);
+		xstream.alias("proveedor", Proveedor.class);
+		xstream.addImplicitCollection(ListaProveedores.class, "proveedores");
 		
-		return proveedores;
+		//Obtengo la lista
+		ListaProveedores lista = (ListaProveedores) xstream.fromXML(new File(ruta));
+		
+		return lista.getProveedores();
 		
 	}
 	
 	public ArrayList<Proveedor> leerProveedoresJSON(String ruta){
 		
 		//Guardamos el Json en un String
-		String fichero = "";
+		String json = "";
 		try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
 		    String linea;
 		    while ((linea = br.readLine()) != null) {
-		        fichero += linea;
+		        json += linea;
 		    }
 
 		} catch (FileNotFoundException e) {
@@ -167,35 +132,12 @@ public class Convertidor {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+		Type listType = new TypeToken<ArrayList<Proveedor>>(){}.getType();
+		ArrayList<Proveedor> proveedores = gson.fromJson(json, listType);
 
-		//Del String "fichero" sacamos el array de proveedores
-		JsonParser parser = new JsonParser();
-		JsonArray gsonArr = parser.parse(fichero).getAsJsonArray();
-		Proveedor prov;
-		ArrayList<Proveedor> proveedores = new ArrayList<>();
-		
-		//Recorremos el array, guardamos los datos en el proveedor y el proveedor en la lista
-		for (JsonElement obj : gsonArr) {
-			JsonObject temp = obj.getAsJsonObject();
-			
-			try {
-				prov = new Proveedor();
-				prov.setCif_proveedor(temp.get("cif_proveedor").getAsString());
-				prov.setFec_homologacion(StringToDate(temp.get("fec_homologacion").getAsString()));
-				prov.setRaz_proveedor(temp.get("raz_proveedor").getAsString());
-				prov.setReg_notarial(temp.get("reg_notarial").getAsInt());
-				prov.setSeg_importe(temp.get("seg_importe").getAsFloat());
-				prov.setSeg_responsabilidad(temp.get("seg_responsabilidad").getAsInt());
-				proveedores.add(prov);
-				
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-			
-		}
-		
 		return proveedores;
-		
 	}
 	
 	public Date StringToDate(String cad) throws ParseException {
@@ -209,7 +151,7 @@ public class Convertidor {
 		ArrayList<Proveedor> proveedores = db.observableToArray(db.listaProveedores());
 		ListaProveedores lista = new ListaProveedores(proveedores);
 		
-		//La convierto a string (formato xml)
+		//Inicializo el XStrem y la convierto a string (formato xml)
 		XStream xstream = new XStream(new DomDriver());
 		xstream.alias("proveedores", ListaProveedores.class);
 		xstream.alias("proveedor", Proveedor.class);
@@ -236,15 +178,10 @@ public class Convertidor {
 		//Obtengo la lista de proveedores
 		AccesoBDD db = new AccesoBDD();
 		ArrayList<Proveedor> proveedores = db.observableToArray(db.listaProveedores());
-		ListaProveedores lista = new ListaProveedores(proveedores);
 		
-		//La convierto a string (formato xml)
-		XStream xstream = new XStream(new JsonHierarchicalStreamDriver());
-		xstream.setMode(XStream.NO_REFERENCES);
-		xstream.alias("proveedores", ListaProveedores.class);
-		xstream.alias("proveedor", Proveedor.class);
-		xstream.addImplicitCollection(ListaProveedores.class, "proveedores");
-		String json = xstream.toXML(lista);
+		//Obtengo un String (formateado en json)
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+		String json = gson.toJson(proveedores);
 		
 		//Escribo el String en el fichero
 		char[] cad = json.toCharArray();
